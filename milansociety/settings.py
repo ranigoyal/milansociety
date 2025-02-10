9fbb6df453251cd9f8843e27a0482627c96f5997
Brij Goyal <brij.goyal@bkgeng.onmicrosoft.com>
2025-02-10 14:05:59 +0000

    Updated ALLOWED_HOSTS in settings.py

--- milansociety/settings.py
+++ milansociety/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'your-secret-key'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['milansociety-production-4fbd.up.railway.app','127.0.0.1', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
