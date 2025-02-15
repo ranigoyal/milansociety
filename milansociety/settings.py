5fbd46f7e548f7418272ed78253dc44e9442edd2
Brij Goyal <brij.goyal@bkgeng.onmicrosoft.com>
2025-02-15 01:19:59 +0000

    Fix deployment configuration

--- milansociety/settings.py
+++ milansociety/settings.py
@@ -125,8 +125,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
